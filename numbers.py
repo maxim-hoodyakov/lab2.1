4e350dd503f54dbb3e6a8610681b2027a584c8b9
Максим <max.hoodyakov@gmail.com>
2023-08-26 12:04:05 +0300

    numbers.py

---
+++ numbers.py
@@ -0,0 +1,10 @@
+print("Write four numbers: ")
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+q = a + b
+w = c + d
+e = float(q/w)
+print("Results: %.2f" % e)
+input('Press ENTER to exit')
